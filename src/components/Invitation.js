6d75d2ae4aa9f8abe98546902b3eb951382006b0
lattrellsapon <lattrellsapon@gmail.com>
2019-07-23 20:57:17 +1200

    Fixed the video error and added two buttons to turn sound on or off for the music

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 export class Invitation extends Component {
+  hideTurnOnButton = () => {
+    return {
+      display: this.props.isSoundOff ? 'block' : 'none'
+    };
+  };
+  hideTurnOffButton = () => {
+    return {
+      display: this.props.isSoundOff ? 'none' : 'block'
+    };
+  };
+
   render() {
     return (
       <div className='invitation-content'>
@@ -9,6 +20,18 @@ export class Invitation extends Component {
         <p>August 24 2019 at 7:00 PM</p>
         <p>RSVP to Shawn by August 18 2019</p>
         <p>Email Shawn at test@gmail.com</p>
+        <div className='sound-buttons'>
+          <div style={this.hideTurnOnButton()}>
+            <button onClick={this.props.turnSoundOn}>
+              <i class='fas fa-volume-up' id='soundItem' />
+            </button>
+          </div>
+          <div style={this.hideTurnOffButton()}>
+            <button onClick={this.props.turnSoundOff}>
+              <i class='fas fa-volume-mute' id='soundItem' />
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -3,13 +3,40 @@ import ShowcaseVideo from '../videos/showcase.mp4';
 import Invitation from './Invitation';
 
 export class Showcase extends Component {
+  state = {
+    isSoundOff: true
+  };
+
+  turnSoundOn = () => {
+    this.setState({
+      isSoundOff: false
+    });
+
+    const video = this.refs.mainVideo;
+
+    video.muted = false;
+  };
+
+  turnSoundOff = () => {
+    this.setState({
+      isSoundOff: true
+    });
+    const video = this.refs.mainVideo;
+
+    video.muted = true;
+  };
+
   render() {
     return (
       <div>
-        <video autoPlay loop id='showcase-video'>
+        <video autoPlay muted loop id='showcase-video' ref='mainVideo'>
           <source src={ShowcaseVideo} type='video/mp4' />
         </video>
-        <Invitation />
+        <Invitation
+          turnSoundOn={this.turnSoundOn}
+          turnSoundOff={this.turnSoundOff}
+          isSoundOff={this.state.isSoundOff}
+        />
       </div>
     );
   }
