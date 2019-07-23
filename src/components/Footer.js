8dd43089ffb70e692d1b2a889ea8341fefaca339
lattrellsapon <lattrellsapon@gmail.com>
2019-07-23 21:05:39 +1200

    Updated the treezy web design link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export class Footer extends Component {
           Created By{' '}
           <a
             className='treezy'
-            href='https://www.instagram.com/lattrellwebdesign/'
+            href='https://www.treezywebdesign.com/'
             target='_blank'
           >
             Treezy Web Desgin
